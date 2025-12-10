d126ca5d5113d8197977e6a2d0de7d2defc2a388
marcy <69540471+reeesespuffs@users.noreply.github.com>
2025-12-10 06:58:41 -0500

    chore: remove legacy app tooltip from serverlist
    Signed-off-by: marcy <69540471+reeesespuffs@users.noreply.github.com>

--- packages/client/src/interface/navigation/servers/ServerList.tsx
+++ packages/client/src/interface/navigation/servers/ServerList.tsx
@@ -169,11 +169,6 @@ export const ServerList = (props: Props) => {
           </a>
           <UserMenu anchor={menuButton} />
         </Tooltip>
-        <Tooltip placement="right" content="Switch back to legacy app">
-          <a href="https://app.revolt.chat" class={entryContainer()}>
-          <Symbol>history</Symbol>
-          </a>
-        </Tooltip>
         <For each={props.unreadConversations.slice(0, 9)}>
           {(conversation) => (
             <Tooltip placement="right" content={conversation.displayName}>
